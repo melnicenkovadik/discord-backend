7ab9ec3f8573d871074a87708ec76c5e15e45b6c
melnicenkovadik <melnicenkovadik@gmail.com>
2022-10-22 22:08:54 +0200

    update Access

--- server.js
+++ server.js
@@ -12,20 +12,13 @@ const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-app.use(cors());
-// allow any cors origin for now
+
+app.use(cors((req, callback) => {
+  callback(null, {
+    origin: true,
+    useSuccessStatus: 200,
+  });
+}));
 
 
 // register the routes
